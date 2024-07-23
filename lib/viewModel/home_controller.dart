e2082600a369dfab6c3fcaeafafe34f7e450dce8
602log <80696631+602log@users.noreply.github.com>
2024-07-23 18:30:27 +0900

    refator: 이미지 수정

--- lib/viewModel/home_controller.dart
+++ lib/viewModel/home_controller.dart
@@ -21,7 +21,7 @@ class HomeController extends GetxController with GetSingleTickerProviderStateMix
     imgList = [
       'https://mblogthumb-phinf.pstatic.net/MjAyMDA5MTdfMzAg/MDAxNjAwMzMxNTU5NzA3.G-oRFcjSLEaao9SvPRkLVBrfjwhOGDngTombKkYM3UQg.hHv2b_kHMCiIVcXygyOR1lddrT--nmmCvvunAD7vr6Eg.PNG.ms1084200/1600331558204.png?type=w800',
       'https://img.gqkorea.co.kr/gq/2023/11/style_6563f59d3588d.jpg',
-      'https://mblogthumb-phinf.pstatic.net/MjAyMDA5MTdfMTkz/MDAxNjAwMzMxNTYyNzMw.ohYzDaVSRNWt3OXq2IJtZ9iUohyp7Icez-qcTHhIpbog.gY8MDfxc9ionOQdR1BNJLMkYG5xpMd9lRbgTyqbr5zgg.PNG.ms1084200/1600331561151.png?type=w800',
+      'https://search4.kakaocdn.net/argon/656x0_80_wr/FiNfMQj2gCD',
       'https://mblogthumb-phinf.pstatic.net/MjAyMDA5MTdfMjk3/MDAxNjAwMzMxNTY1MDY1.H0syWuE7teorucB-FRiBYlhZ5ij0dF211gz3e3-Zr2Qg.9B4XFZSLoWkKboZnJmvnuxFfgg2YB23Udtu7jJ0z3tog.PNG.ms1084200/1600331562995.png?type=w800',
       'https://mblogthumb-phinf.pstatic.net/MjAyMDA5MTdfMjU2/MDAxNjAwMzMxNTY2MTkx.NnoMxLJ4X_nMqgfCAKu9YmdhpKku9RN8CegyEjcPpkUg.71gVBOZdJ3dEK_se62HBqTkWnD2luU0cQXNXIb-dxucg.PNG.ms1084200/1600331564541.png?type=w800',
       'https://mblogthumb-phinf.pstatic.net/MjAyMDA5MTdfNTYg/MDAxNjAwMzMxNTY3NDM0.CXDtSNJFkCGskctHgZWjiTTbl3dZcRsODTJw63vnpwUg.a-NkKUSt-FGx0ab1XZgE5s-lBrNySh4MaVKMwmQ5feAg.PNG.ms1084200/1600331565684.png?type=w800',
