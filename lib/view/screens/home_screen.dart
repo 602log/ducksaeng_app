d17650c5b9d9d9cdf2e437e1d8a3912f15b2d05c
602log <80696631+602log@users.noreply.github.com>
2024-07-23 20:56:33 +0900

    refactor: 미사용 코드 삭제

--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -34,13 +34,16 @@ class HomeScreen extends GetView<HomeController> {
                   child: Column(
                     children: [
                     Expanded(
+                      // 최애 사진 슬라이더
+                      // todo: 최애 등록이 없는 경우 빈 칸 표출, 최애 사진 누르면 생일 리스트 조회 페이지로 이동
                       child: CarouselSlider(
                           items: controller.imgWidget,
                           options: CarouselOptions(
                             autoPlay: false,
                             enlargeCenterPage: true,
                             enableInfiniteScroll: false,
-                              aspectRatio: 1
+                            aspectRatio: 1,
+                            viewportFraction: 0.7,
                           ),
                         ),
                     ),
@@ -65,95 +68,5 @@ class HomeScreen extends GetView<HomeController> {
           bottomNavigationBar: const CustomButtomNavigationBar(),
         )
     );
-
-
-    // return MediaQuery(
-    //   data: MediaQuery.of(context).copyWith(textScaler: TextScaler.noScaling),
-    //   child: Scaffold(
-    //     appBar: MainAppBarWidget(),
-    //     body: SafeArea(
-    //       child: Stack(
-    //         children: [
-    //           Column(
-    //             children: [
-    //               Expanded(
-    //                 flex: 3,
-    //                 child: Container(
-    //                     decoration: BoxDecoration(
-    //                       color: Colors.grey.shade200,
-    //                       borderRadius: BorderRadius.only(
-    //                         bottomLeft: Radius.circular(90),
-    //                         bottomRight: Radius.circular(90),
-    //                       ),
-    //                     ),
-    //                     child: Column(
-    //                       crossAxisAlignment: CrossAxisAlignment.stretch,
-    //                       children: [
-    //
-    //                         // 최애 사진 슬라이더
-    //                         // todo: 최애 등록이 없는 경우 빈 칸 표출, 최애 사진 누르면 생일 리스트 조회 페이지로 이동
-    //                         Expanded(
-    //                           flex: 3,
-    //                           child: CarouselSlider(
-    //                             items: controller.imgWidget,
-    //                             options: CarouselOptions(
-    //                               autoPlay: false,
-    //                               enlargeCenterPage: true,
-    //                               enableInfiniteScroll: false,
-    //                                 aspectRatio: 16/20
-    //                             ),
-    //                           ),
-    //                         ),
-    //
-    //                         const Expanded(
-    //                           flex: 1,
-    //                           child: SizedBox(),
-    //                         ),
-    //                       ],
-    //                     )),
-    //               ),
-    //               const Expanded(
-    //                 flex: 2,
-    //                 child: SizedBox(),
-    //               ),
-    //             ],
-    //           ),
-    //
-    //           Column(
-    //             children: [
-    //               Expanded(
-    //                 flex: 3,
-    //                 child: Align(
-    //                   alignment: Alignment.bottomCenter,
-    //                   child: Row(
-    //                     children: [
-    //
-    //                       // 아이돌 서치 필드
-    //                       Expanded(
-    //                         child: Padding(
-    //                           padding:
-    //                               const EdgeInsets.symmetric(horizontal: 35.0),
-    //                           child: SearchTextFormFieldWidget(
-    //                             callback: () {},
-    //                           ),
-    //                         ),
-    //                       ),
-    //
-    //                     ],
-    //                   ),
-    //                 ),
-    //               ),
-    //               const Expanded(
-    //                 flex: 2,
-    //                 child: SizedBox(),
-    //               ),
-    //             ],
-    //           ),
-    //         ],
-    //       ),
-    //     ),
-    //     bottomNavigationBar: const CustomButtomNavigationBar(),
-    //   ),
-    // );
   }
 }
