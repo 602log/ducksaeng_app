5266171c5b1aae0edbae1e6167558e7e007f9b10
602log <80696631+602log@users.noreply.github.com>
2024-08-07 05:45:36 +0900

    design: 내가 등록한 이벤트 화면 수정

--- lib/view/screens/my_event_list_screen.dart
+++ lib/view/screens/my_event_list_screen.dart
@@ -6,6 +6,7 @@ import 'package:get/get.dart';
 import '../../viewModel/my_event_controller.dart';
 import '../components/custom_bottom_navigation_bar.dart';
 import '../components/search_and_navigation_bar_widget.dart';
+import '../components/search_text_form_field_widget.dart';
 
 /// 내가 등록한 생일카페 리스트 화면
 class MyEventListScreen extends GetView<MyEventController> {
@@ -14,26 +15,84 @@ class MyEventListScreen extends GetView<MyEventController> {
     return MediaQuery(
       data: MediaQuery.of(context).copyWith(textScaler: TextScaler.noScaling),
       child: Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          title: SearchAndNavigationBarWidget(),
-        ),
         body: SafeArea(
           child: Stack(
             children: [
               SingleChildScrollView(
                 child: Padding(
-                  padding: const EdgeInsets.all(16),
+                  padding: const EdgeInsets.only(
+                    left: 16,
+                    right: 16,
+                    top: 80,
+                  ),
                   child: Align(
                     alignment: Alignment.center,
                     child: Wrap(
                       spacing: 20,
                       runSpacing: 15,
                       children: List.generate(15, (index) {
-                        return MyEventListItemWidget();
+                        return const MyEventListItemWidget();
                       }),
                     ),
                   ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 16,
+                  right: 16,
+                  top: 10,
+                ),
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(45),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 5,
+                              offset: Offset(1, 3),
+                            ),
+                          ],
+                        ),
+                        child: IconButton(
+                          onPressed: () {
+                            Get.back();
+                          },
+                          icon: const Icon(
+                            Icons.arrow_back_ios_new,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(45),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Colors.grey,
+                                blurRadius: 5,
+                                offset: Offset(1, 3),
+                              ),
+                            ],
+                          ),
+                          child: SearchTextFormFieldWidget(
+                            borderSide: BorderSide.none,
+                            callback: () {},
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
                 ),
               ),
               Padding(
@@ -70,7 +129,7 @@ class MyEventListScreen extends GetView<MyEventController> {
             ],
           ),
         ),
-        bottomNavigationBar: CustomButtomNavigationBar(),
+        bottomNavigationBar: const CustomButtomNavigationBar(),
       ),
     );
   }
